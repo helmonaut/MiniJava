e537c6b4a384fc8b98515cfb1a1491cf1bcc821b
Tim Habermaas <tim.habermaas@gmail.com>
2011-04-23 01:43:07 +0200

    refactored keywords map creation

--- src/edu/kit/pp/minijava/tokens/Keyword.java
+++ src/edu/kit/pp/minijava/tokens/Keyword.java
@@ -1,4 +1,5 @@
 package edu.kit.pp.minijava.tokens;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Collections;
@@ -8,62 +9,22 @@ public class Keyword extends Token {
     public static final Map<String, Boolean> _KEYWORDS = createMap();
 
     private static Map<String, Boolean> createMap() {
+	String[] keywords = {"abstract", "assert", "boolean", "break", "byte",
+	    "case", "catch", "char", "class", "const", "continue", "default",
+	    "double", "do", "else", "enum", "extends", "false", "finally",
+	    "final", "float", "for", "goto", "if", "implements", "import",
+	    "instanceof", "interface", "int", "long", "native", "new", "null",
+	    "package", "private", "protected", "public", "return", "short",
+	    "static", "strictfp", "super", "switch", "synchronized", "this",
+	    "throws", "throw", "transient", "true", "try", "void", "volatile",
+	    "while"};
 
-        Map<String, Boolean> aMap = new HashMap<String, Boolean>(); 
-        aMap.put("abstract", true);
-        aMap.put("assert", true);
-        aMap.put("boolean", true);
-        aMap.put("break", true);
-        aMap.put("byte", true);
-        aMap.put("case", true);
-        aMap.put("catch", true);
-        aMap.put("char", true);
-        aMap.put("class", true);
-        aMap.put("const", true);
-        aMap.put("continue", true);
-        aMap.put("default", true);
-        aMap.put("double", true);
-        aMap.put("do", true);
-        aMap.put("else", true);
-        aMap.put("enum", true);
-        aMap.put("extends", true);
-        aMap.put("false", true);
-        aMap.put("finally", true);
-        aMap.put("final", true);
-        aMap.put("float", true);
-        aMap.put("for", true);
-        aMap.put("goto", true);
-        aMap.put("if", true);
-        aMap.put("implements", true);
-        aMap.put("import", true);
-        aMap.put("instanceof", true);
-        aMap.put("interface", true);
-        aMap.put("int", true);
-        aMap.put("long", true);
-        aMap.put("native", true);
-        aMap.put("new", true);
-        aMap.put("null", true);
-        aMap.put("package", true);
-        aMap.put("private", true);
-        aMap.put("protected", true);
-        aMap.put("public", true);
-        aMap.put("return", true);
-        aMap.put("short", true);
-        aMap.put("static", true);
-        aMap.put("strictfp", true);
-        aMap.put("super", true);
-        aMap.put("switch", true);
-        aMap.put("synchronized", true);
-        aMap.put("this", true);
-        aMap.put("throws", true);
-        aMap.put("throw", true);
-        aMap.put("transient", true);
-        aMap.put("true", true);
-        aMap.put("try", true);
-        aMap.put("void", true);
-        aMap.put("volatile", true);
-        aMap.put("while", true);
-        return Collections.unmodifiableMap(aMap);
+	Map<String, Boolean> aMap = new HashMap<String, Boolean>();
+	for (String keyword : keywords) {
+	    aMap.put(keyword, true);
+	}
+
+	return Collections.unmodifiableMap(aMap);
     }
 
     public Keyword(String value) {
