543f91a09660b31398a6bac215cde5af196ef337
Tim Habermaas <tim.habermaas@gmail.com>
2011-04-28 17:23:13 +0200

    added FixedQueue for token lookahead

---
+++ test/edu/kit/pp/minijava/util/FixedQueueTest.java
@@ -0,0 +1,66 @@
+package edu.kit.pp.minijava.util;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FixedQueueTest {
+
+	private FixedQueue<Integer> _queue;
+
+	public FixedQueueTest() {
+	}
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() {
+		_queue = new FixedQueue<Integer>(3);
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void addShouldAddItemsToTheBeginning() {
+		_queue.add(4);
+		assertEquals(4, (int)_queue.get(0));
+		_queue.add(5);
+		assertEquals(5, (int)_queue.get(0));
+	}
+
+	@Test
+	public void addShouldMoveItemsToTheEnd() {
+		_queue.add(4);
+		_queue.add(5);
+		assertEquals(4, (int)_queue.get(1));
+	}
+
+	@Test(expected=Exception.class)
+	public void shouldNotSaveMoreThan3Items() {
+		_queue.add(4);
+		_queue.add(5);
+		_queue.add(6);
+		_queue.add(7);
+		_queue.get(3);
+	}
+
+	@Test
+	public void addShouldKickOldValuesOut() {
+		_queue.add(4);
+		_queue.add(5);
+		_queue.add(6);
+		_queue.add(7);
+		assertEquals(5, (int)_queue.get(2));
+	}
+}
