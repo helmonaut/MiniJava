95e11a4aa6b95775602bac1c7899b93321078326
Tim Habermaas <tim.habermaas@gmail.com>
2011-04-29 22:01:09 +0200

    buh

--- src/edu/kit/pp/minijava/ast/Program.java
+++ src/edu/kit/pp/minijava/ast/Program.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.kit.pp.minijava.ast;
 
-/**
- *
- * @author tim
- */
 public class Program extends Node {
 
 }
