0bb4ddb9543073f20366e9a5600a6b9c2dd71a82
Tim Habermaas <tim.habermaas@gmail.com>
2011-05-05 23:15:26 +0200

    ClassDeclaration explicitly saves members by type now.

--- src/edu/kit/pp/minijava/ast/ClassDeclaration.java
+++ src/edu/kit/pp/minijava/ast/ClassDeclaration.java
@@ -7,15 +7,38 @@ import java.util.List;
 public class ClassDeclaration extends Node {
 
 	private Identifier _name;
-	private List<ClassMember> _members;
+	private List<MainMethod> _mainMethods;
+	private List<Method> _methods;
+	private List<Field> _fields;
 
 	public ClassDeclaration(Identifier name) {
 		_name = name;
-		_members = new LinkedList<ClassMember>();
+		_mainMethods = new LinkedList<MainMethod>();
+		_methods = new LinkedList<Method>();
+		_fields = new LinkedList<Field>();
 	}
 
 	public void add(ClassMember member) {
-		_members.add(member);
+		if (member instanceof Field) {
+			add((Field) member);
+		}
+		else if (member instanceof Method) {
+			add((Method) member);
+		}
+		else if (member instanceof MainMethod) {
+			add((MainMethod) member);
+		}
 	}
 
+	public void add(Field field) {
+		_fields.add(field);
+	}
+
+	public void add(Method method) {
+		_methods.add(method);
+	}
+
+	public void add(MainMethod mainMethod) {
+		_mainMethods.add(mainMethod);
+	}
 }
