fa848b0ae4c206e68700a773b501c380bc0f070e
Willi Nagel <willi.nagel@googlemail.com>
2011-05-06 15:48:08 +0200

    interface NodeVisitor

---
+++ src/edu/kit/pp/minijava/NodeVisitor.java
@@ -0,0 +1,24 @@
+package edu.kit.pp.minijava;
+
+import edu.kit.pp.minijava.ast.*;
+
+public interface NodeVisitor {
+	void processProgram(Program prog);
+	void processClassDeclaration(ClassDeclaration classDecl);
+	void processClassMember(ClassMember classMember);
+	void processMainMethod(MainMethod mainMethod);
+	void processMethod(Method method);
+	void processField(Field field);
+	void processParameters(Parameters params);
+	void processParameter(Parameter param);
+	void processType(Type type);
+	void processBasicType(BasicType basType);
+	void processBlock(Block block);
+	void processBlockStatement(BlockStatement blockStmt);
+	void processLocalVariableDeclarationStatement(LocalVariableDeclarationStatement locVarDeclStmt);
+	void processExpression(Expression expr);
+	void processBinaryExpression(BinaryExpression binExpr);
+	void processPostfixExpression(PostfixExpression postfixExpr);
+	void processPrimaryExpression(PrimaryExpression primExp);
+	void processUnaryExpression(UnaryExpression unaryExp);
+}
